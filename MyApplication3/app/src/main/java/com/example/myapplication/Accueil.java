fb7e89221b69f51166b00d97f8d831ba7e988892
daniellazer2902 <daniellazer2902maker@gmail.com>
2020-11-25 15:14:08 +0100

    Login fonctionnel sans les visuel avec passage de variable

--- MyApplication3/app/src/main/java/com/example/myapplication/Accueil.java
+++ MyApplication3/app/src/main/java/com/example/myapplication/Accueil.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -17,6 +18,7 @@ import com.android.volley.toolbox.Volley;
 
 import org.json.JSONException;
 import org.json.JSONObject;
+import org.w3c.dom.Text;
 
 public class Accueil extends AppCompatActivity {
 
@@ -25,6 +27,11 @@ public class Accueil extends AppCompatActivity {
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.accueil);
+        TextView text = (TextView)findViewById(R.id.textConnect);
+
+        Intent myIntent = getIntent(); // gets the previously created intent
+        String firstKeyName = myIntent.getStringExtra("pseudo");
+        text.setText(firstKeyName);
     }
 
 
